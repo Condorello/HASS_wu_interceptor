569575147d4977f937067f69f37b23dd996adec2
Condorello <andreanottoli@gmail.com>
2023-08-28 16:19:30 +0200

    included variables

--- HASS_addon/run.sh
+++ HASS_addon/run.sh
@@ -1,4 +1,16 @@
-#|/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
+
+MQTT_BROKER_HOST=$(bashio::config 'mqtt_broker_host')
+export MQTT_BROKER_HOST
+
+MQTT_BROKER_PORT=$(bashio::config 'mqtt_broker_port')
+export MQTT_BROKER_PORT
+
+MQTT_USERNAME=$(bashio::config 'mqtt_username')
+export MQTT_USERNAME
+
+MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+export MQTT_PASSWORD
 
 echo "running pws server"
 
